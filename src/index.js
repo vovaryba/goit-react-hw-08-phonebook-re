9eb11d5e5e13018de58fd85286b44c688f80cdaf
vovaryba <vovaryba@gmail.com>
2022-05-22 23:02:21 +0300

    Material start

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import { PersistGate } from 'redux-persist/integration/react';
 import App from './App';
 import { store, persistor } from 'redux/store';
 import './index.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,4 +1,5 @@
-import { useForm } from 'react-hook-form';
+import { useForm, Controller } from 'react-hook-form';
+import { Typography, TextField, Button } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { authOperations } from 'redux/auth';
 import s from './LoginPage.module.css';
@@ -7,8 +8,8 @@ const LoginPage = () => {
   const dispatch = useDispatch();
 
   const {
-    register,
     handleSubmit,
+    control,
     reset,
     formState: { errors },
   } = useForm({
@@ -25,54 +26,79 @@ const LoginPage = () => {
   };
 
   return (
-    <>
-      <h2>Authorization page</h2>
+    <div className={s.page}>
+      <Typography variant="h2" component="div" gutterBottom>
+        Authorization page
+      </Typography>
       <form
         onSubmit={handleSubmit(onSubmitForm)}
         className={s.form}
         autoComplete="off"
       >
-        <label className={s.label}>
-          Email
-          <input
-            className={s.input}
-            type="email"
-            name="email"
-            {...register('email', {
-              required: { value: true, message: 'Required' },
-              pattern: {
-                value:
-                  /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
-                message: 'Enter the correct email',
-              },
-            })}
-          />
-        </label>
-        {errors.email && <p className={s.error}>{errors.email.message}</p>}
-        <label className={s.label}>
-          Password
-          <input
-            className={s.input}
-            type="password"
-            name="password"
-            {...register('password', {
-              required: { value: true, message: 'Required' },
-              pattern: {
-                value: /^(?=.*[0-9])(?=.*[a-z]).{6,}/,
-                message:
-                  'Password must contain at least 6 lowercase letters and numbers',
-              },
-            })}
-          />
-        </label>
-        {errors.password && (
-          <p className={s.error}>{errors.password.message}</p>
-        )}
-        <button type="submit" className={s.button}>
+        <Controller
+          control={control}
+          name="email"
+          rules={{
+            required: { value: true, message: 'Required' },
+            pattern: {
+              value:
+                /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/,
+              message: 'Enter the correct email',
+            },
+          }}
+          render={({ field }) => (
+            <TextField
+              margin="dense"
+              label="Email"
+              type="email"
+              name="email"
+              fullWidth={true}
+              value={field.value}
+              onChange={e => field.onChange(e)}
+              error={!!errors.email?.message}
+              helperText={errors.email?.message}
+            />
+          )}
+        />
+        <Controller
+          control={control}
+          name="password"
+          rules={{
+            required: { value: true, message: 'Required' },
+            pattern: {
+              value: /^(?=.*[0-9])(?=.*[a-z]).{6,}/,
+              message:
+                'Password must contain at least 6 lowercase letters and numbers',
+            },
+          }}
+          render={({ field }) => (
+            <TextField
+              margin="dense"
+              label="Password"
+              type="password"
+              name="password"
+              fullWidth={true}
+              value={field.value}
+              onChange={e => field.onChange(e)}
+              error={!!errors.password?.message}
+              helperText={errors.password?.message}
+            />
+          )}
+        />
+
+        <Button
+          color="primary"
+          variant="contained"
+          type="submit"
+          fullWidth={true}
+          disableElevation={true}
+          sx={{ marginTop: 2 }}
+          onClick={() => dispatch(authOperations.logOut())}
+        >
           Log in
-        </button>
+        </Button>
       </form>
-    </>
+    </div>
   );
 };
 
